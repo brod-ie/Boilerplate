846e22db4581683a1fc1832b7f4ad7eaa3e25c21
asos <asos@Ryans-MacBook-Pro-2.local>
2014-08-04 16:02:06 +0100

    Compiled partials

---
+++ public/templates/layout.js
@@ -0,0 +1,7 @@
+module.exports = function template(locals) {
+var buf = [];
+var jade_mixins = {};
+var jade_interp;
+;var locals_for_with = (locals || {});(function (title, version) {
+buf.push("<!DOCTYPE html><html><head><title>" + (jade.escape((jade_interp =  title ) == null ? '' : jade_interp)) + "</title><link rel=\"stylesheet\" type=\"text/css\" href=\"http://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,700,700italic\"><link rel=\"stylesheet\" type=\"text/css\"" + (jade.attr("href", '/styles/main.css?' + ( version ) + '', true, true)) + "></head><body></body></html>");}("title" in locals_for_with?locals_for_with.title:typeof title!=="undefined"?title:undefined,"version" in locals_for_with?locals_for_with.version:typeof version!=="undefined"?version:undefined));;return buf.join("");
+};
